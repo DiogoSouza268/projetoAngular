bde9adb87fd711d64fe74d42a98a8a7a377ed951
DiogoSouza268 <dooss2085@gmail.com>
2026-08-28 09:36:56 -0300

    refatoração do menu e ajustes de estilo: implementação do overlay, melhorias na navegação e correção de estilos globais

--- src/app/componentes/menu/menu.ts
+++ src/app/componentes/menu/menu.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
@@ -13,7 +15,5 @@ export class Menu {
   aparecMenu(){
 
     this.aparecaMenu = !this.aparecaMenu;
-
   }
-
 }
